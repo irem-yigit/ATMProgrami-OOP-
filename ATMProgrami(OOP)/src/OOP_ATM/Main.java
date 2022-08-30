695c96290b8da84eddcbc0072a8b8a03613a9f93
İrem Yiğit <51033713+irem-yigit@users.noreply.github.com>
2022-08-30 18:14:21 +0300

    Update Main.java

--- ATMProgrami(OOP)/src/OOP_ATM/Main.java
+++ ATMProgrami(OOP)/src/OOP_ATM/Main.java
@@ -1,19 +1,10 @@
 package OOP_ATM;
 
 public class Main {
-    public static void main(String[] args){ //Object Oriented (Nesne yönelimli) ATM programı
-        //Login Class'ı kullanıcı girişini kontrol edecek.
-        //Hesap Class'ı hesap işlemlerini yapacak.
-        //ATM Class'ı ise Atm'mizi çalıştıracak.
-
+    public static void main(String[] args){
         ATM atm=new ATM();
-
         Hesap hesap = new Hesap("iremyigit","12345",5000 );
-
         atm.calis(hesap);
         System.out.println("Programdan çıkılıyor...");
-
-
-
     }
 }
